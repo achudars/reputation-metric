342433813fb02d95eaf5804a0b1d3ceaa5ff83e3
Aleks <achudars@gmail.com>
2015-03-03 18:53:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -312,8 +312,8 @@ app.controller("AppCtrl", function ($scope) {
 		var numberOfAttacks = $scope.model.magnitude.min;
 		console.clear();
 		console.log("_______________________________________________");
-		console.log($scope.model.total + "," + $scope.model.data);
-		console.log($scope.model.magnitude.min + "," + $scope.model.magnitude.max);
+		console.log("Input: " + $scope.model.total + "," + $scope.model.data);
+		console.log("Magnitude: " + $scope.model.magnitude.min + "," + $scope.model.magnitude.max);
 		console.warn("[ " + $scope.model.metricsOfExpanded + " ]");
 		console.dir("Self-promoting [ i, δ1, δ2, δ3, δ4, δ5, δ6 ]: ");
 
